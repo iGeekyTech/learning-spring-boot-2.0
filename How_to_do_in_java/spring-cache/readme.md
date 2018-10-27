52b49082089c01bcbd885c791dc5903f2e08cd8a
JavaProgramer <prateek.ashtikar512@gmail.com>
2018-10-27 22:30:45 +0530

    Spring boot caching
    Spring boot caching

---
+++ How_to_do_in_java/spring-cache/readme.md
@@ -0,0 +1,4 @@
+#Spring boot caching tutorial with example
+
+In this Spring boot tutorial, learn to easily manage application cache from Spring framework caching support. Spring has some nice features around caching and the abstraction on the spring cache API is very much simple to use.
+
---
+++ How_to_do_in_java/spring-cache/src/main/java/com/example/springcache/domain/Student.java
@@ -0,0 +1,37 @@
+package com.example.springcache.domain;
+
+public class Student {
+	private String id;
+	private String name;
+	private String clz;
+
+	public Student(String id, String name, String clz) {
+		this.id = id;
+		this.name = name;
+		this.clz = clz;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClz() {
+		return clz;
+	}
+
+	public void setClz(String clz) {
+		this.clz = clz;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
